b96c1fb6301d7354d0579817adf5b75d9ea77b8b
Joana Moreira <joanamoreira989@gmail.com>
2018-04-30 13:53:28 +0100

    Initial variables

--- js/app.js
+++ js/app.js
@@ -24,10 +24,22 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function() {
+    this.sprite = 'images/char-cat-girl.png'
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+let allEnemies = [];
 // Place the player object in a variable called player
 
 
